76e2e56a75e86b25806d7b70e76e8ac5e4b5e244
Giadagenovesi <giada.g@outlook.it>
2023-09-14 16:32:22 +0200

    Creata la classe address

---
+++ csharp-lista-indirizzi/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Address
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Zip { get; set; }
+
+        public Address(string name, string surname, string street, string city, string province, string zip) 
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.Zip = zip;
+        }
+
+        //public override string ToString()
+        //{
+        //    return $"Indirizzo di {this.Name} {this.Surname}: {this.Street} {this.City} {this.Province} {this.Zip}";
+        //}
+
+    }
+}
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -4,7 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Address> listAddress = new List<Address>();
+
+            try
+            {
+
+                StreamReader fileListAddresss = File.OpenText("C:\\Users\\giada\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\my-addresses.csv");
+
+                int lineNumber = 0;
+                while (!fileListAddresss.EndOfStream)
+                {
+                    string line = fileListAddresss.ReadLine();
+                    lineNumber++;
+                    if (lineNumber > 1)
+                    {
+                        string[] stringSplits = line.Split(',');
+
+                        if (stringSplits.Length < 6)
+                        {
+                            Console.WriteLine($"L'indirizzo {line} non è leggibile");
+                        }
+                        else
+                        {
+
+                            string name = stringSplits[0];
+                            string surname = stringSplits[1];
+                            string street = stringSplits[2];
+                            string city = stringSplits[3];
+                            string province = stringSplits[4];
+                            string zip = stringSplits[5];
+
+                            Console.WriteLine($" {name} {surname} {street} {city} {province} {zip}");
+
+                            Address newAddress = new Address(name, surname, street, city, province, zip);
+
+                            listAddress.Add(newAddress);
+                        }
+                    }
+                }
+
+                //Console.WriteLine(listAddress);
+                fileListAddresss.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                StreamWriter fileToWrite = File.CreateText("C:\\Users\\giada\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\mylistaddress.csv");
+
+                for (int i = 0; i < listAddress.Count; i++)
+                {
+                    Console.WriteLine(listAddress[i]);
+                    fileToWrite.WriteLine(listAddress[i]);
+                }
+                fileToWrite.Close();
+            }catch
+            {
+                Console.WriteLine("Non è stato possibile stampare l'indirizzo");
+            }
+
         }
     }
 }
